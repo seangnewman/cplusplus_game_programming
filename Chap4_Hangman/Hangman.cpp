088d5335c06414dfd8fd25936cd86ebde10d1d9a
Sean Newman <seangnewman@msn.com>
2018-09-08 17:57:04 -0700

    Completed Hangman

--- Chap4_Hangman/Hangman.cpp
+++ Chap4_Hangman/Hangman.cpp
@@ -8,4 +8,77 @@
 #include <cctype>
 
 using namespace std;
+
+int main() {
+
+	//setup
+	const int MAX_WRONG = 8;				// Maximum number of incorrect guesses allowed
+
+	vector<string> words;					// collection of possible words to guess
+	words.push_back("GUESS");
+	words.push_back("HANGMAN");
+	words.push_back("DIFFICULT");
+
+	srand( static_cast<unsigned int>(time(0)));
+	random_shuffle(words.begin(), words.end());
+
+	const string THE_WORD = words[0];		// word to guess
+	int wrong = 0;							// number of incorrect guesses
+	string soFar(THE_WORD.size(), '-');
+	string used = "";						// letter already guessed
+
+	cout << "Welcome to Hangman. Good luck!\n";
+
+	// main loop
+	while ((wrong < MAX_WRONG) && (soFar != THE_WORD))
+	{
+		cout << "\n\nYou have " << (MAX_WRONG - wrong);
+		cout << " incorrect guesses left.\n";
+		cout << "\nYou've used the following letters:\n" << used << endl;
+		cout << "\nSo far, the word is:\n" << soFar << endl;
+
+		char guess;
+		cout << "\n\nEnter your guess: ";
+		cin >> guess;
+		guess = toupper(guess);				// Match upppercase 
+		while (used.find(guess) != string::npos) {
+			cout << "\nYou've already guessed " << guess << endl;
+			cout << "Enter your guess:";
+			cin >> guess;
+			guess = toupper(guess);
+		}
+			
+		used += guess;
+
+		if (THE_WORD.find(guess) != string::npos) {
+			cout << "That's right! " << guess << " is in the workd.\n";
+
+			// update soFar to include newly guessed letter
+			for (int i = 0; i < THE_WORD.length() ; ++i)
+			{
+				if (THE_WORD[i] == guess) {
+					soFar[i] = guess;
+				}
+			}
+		}
+		else {
+			cout << "Sorry, " << guess << " isn't in the word.\n";
+			++wrong;
+		}
+	}
+
+
+	// shut down
+	if (wrong == MAX_WRONG) {
+		cout << "\nYou've been hanged!";
+	}
+	else {
+		cout << "\nYou guessed it!";
+	}
+
+
+	cout << "\nThe word was " << THE_WORD << endl;
+
+	return 0;
+}
  
